22622347f16138d486e54680e0afcfd600dcd4de
HMichaut <hectormichaut@gmail.com>
2021-08-22 19:26:57 +0200

    model finalisation

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,6 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :project
-  has_many :replies
+  has_many :replies, dependent: :delete_all
+  has_one_attached :image, dependent: :destroy
 end
